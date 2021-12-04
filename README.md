f76d9f245074a84a21d3fd3fb2176e970f64a058
JonasHiltl <jonashiltl2003@gmail.com>
2021-12-04 22:18:45 +0100

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Fastify Queue
+
+_Still work in progress_
+
+### Installation:
+
+```
+npm i fastify-queue
+npm i bullmq
+```
+
+This package is an abstraction on top of [BullMQ](https://github.com/taskforcesh/bullmq)
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { FastifyInstance } from "fastify";
+import fp from "fastify-plugin";
+import { Queue, Worker } from "bullmq";
+import IORedis, { RedisOptions } from "ioredis";
+import * as fg from "fast-glob";
+import * as path from "path";
+
+export interface FastifyQueueOptions extends RedisOptions {
+  bullPath: string;
+}
+
+/**
+ * Load every worker function inside a specified directory
+ * @param {Object} fastify - Fastify instance
+ * @param {Object} opts - Plugin's options
+ */
+const fastifyBullMQ = async (
+  fastify: FastifyInstance,
+  opts: FastifyQueueOptions
+) => {
+  const connection = new IORedis(opts);
+  const queues = {};
+  const workers = {};
+
+  const files = fg.sync(opts.bullPath);
+
+  files.forEach(async (filePath) => {
+    const parts = filePath.split("/");
+    const queueName = parts[parts.length - 2];
+
+    const worker = await import(path.resolve(filePath));
+
+    (queues as any)[queueName] = new Queue(queueName, {
+      connection,
+    });
+
+    (workers as any)[queueName] = new Worker(queueName, worker.default, {
+      connection,
+    });
+  });
+
+  fastify.decorate("queues", queues);
+  fastify.decorate("workers", workers);
+};
+
+export default fp<FastifyQueueOptions>(fastifyBullMQ, {
+  name: "fastify-queue",
+});
