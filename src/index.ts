52eb55051d527dbe94ef1a3274361ca97e8ae533
JonasHiltl <jonashiltl2003@gmail.com>
2021-12-12 18:08:51 +0100

    Accept queue and worker config as export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
-import { Queue, Worker, ConnectionOptions } from 'bullmq';
+import { Queue, Worker, ConnectionOptions, QueueOptions } from 'bullmq';
 import * as fg from 'fast-glob';
 import path from 'path';
 
@@ -24,33 +24,43 @@ const fastifyBullMQ = async (
   const files = fg.sync(opts.bullPath);
 
   files.forEach(async (filePath) => {
-    const parts = filePath.split('/');
-    // the queue name is defined by the name of the directory in which the files are
-    const queueName = parts[parts.length - 2];
+    try {
+      const parts = filePath.split('/');
+      // the queue name is defined by the name of the directory in which the files are
+      const queueName = parts[parts.length - 2];
 
-    const worker = await import(path.resolve(filePath));
+      const worker = await import(path.resolve(filePath));
 
-    (queues as any)[queueName] = new Queue(queueName, {
-      connection: opts.connection,
-    });
-    fastify.log.info(`Created a queue called ${queueName}`);
+      const queueConfig: QueueOptions = worker.queueConfig;
+      const workerConfig: WorkerOptions = worker.workerConfig;
 
-    if (
-      Object.keys(worker.default).length === 0 &&
-      worker.default.constructor === Object
-    ) {
-      fastify.log.warn(
-        `The queue ${queueName} does not have a worker function`
-      );
-    } else {
-      (workers as any)[queueName] = new Worker(
-        queueName,
-        (job) => worker.default(fastify, job),
-        {
-          connection: opts.connection,
-        }
-      );
-      fastify.log.info(`Created a worker for the queue ${queueName}`);
+      (queues as any)[queueName] = new Queue(queueName, {
+        connection: opts.connection,
+        ...(queueConfig && queueConfig),
+      });
+      fastify.log.info(`Created the queue ${queueName}`);
+
+      if (
+        worker.default &&
+        Object.keys(worker.default).length === 0 &&
+        worker.default.constructor === Object
+      ) {
+        fastify.log.warn(
+          `The queue ${queueName} does not have a worker function`
+        );
+      } else {
+        (workers as any)[queueName] = new Worker(
+          queueName,
+          (job) => worker.default(fastify, job),
+          {
+            connection: opts.connection,
+            ...(workerConfig && workerConfig),
+          }
+        );
+        fastify.log.info(`Created a worker for the queue ${queueName}`);
+      }
+    } catch (error) {
+      console.log('Cateched error', error);
     }
   });
 
